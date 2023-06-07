9a932ac0c320de23ce5d35a84c66a523aa3e21ab
choisumin <tnals634@naver.com>
2023-06-07 13:53:18 +0900

    이름 중복체크,비밀번호,리뷰 수정

--- js/validationCheck.js
+++ js/validationCheck.js
@@ -1,3 +1,5 @@
+const $comment = document.querySelector("#comment");
+
 let scriptTag = /[~!@#\$%\^&\*\(\)_\+\-={}\[\];:<>,\.\/\?\"\'\/\|\\]/; // 특수문자들
 let space = /\s/; //공백값
 
@@ -10,13 +12,13 @@ export function movieTitleCheck(find) {
     alert("제목을 입력해주세요");
   } else if (space.test(find) == true) {
     //공백이 있는 경우
-    alert("제목에 공백은 사용할 수 없습니다.");
+    alert("공백은 사용할 수 없습니다.");
   } else if (scriptTag.test(find) == true) {
     //특수문자가 포함된 경우
     alert("특수문자는 들어갈 수 없습니다.");
   } else if (find.length > 20) {
     // 20자 초과한 경우
-    alert("제목을 20자 이내로 작성해주세요");
+    alert("20자 이내로 작성해주세요");
   } else {
     check = true;
   }
@@ -27,21 +29,40 @@ export function movieTitleCheck(find) {
 export function ReviewIdCheck(reviewId) {
   let check = false;
 
+  //localStorage에서 같은 이름이 있나 확인
+  if (localStorage.length != 0) {
+    let key = localStorage.key(0);
+    //키에 해당하는 값을 get...에 넣어준다
+    let getLocalStorage = JSON.parse(localStorage.getItem(key));
+
+    for (let i = 0; i < getLocalStorage.length; i++) {
+      //getLocalStorage = [{name: name,...},{},...];
+      if (getLocalStorage[i].name == reviewId) {
+        check = 2;
+        break;
+      }
+    }
+  }
+
   if (reviewId.length == 0) {
     //값이 없는 경우
-    alert("아이디를 입력해주세요");
+    alert("이름을 입력해주세요");
+  } else if (check == 2) {
+    //localStorage에 값이 있는경우
+    alert(`${reviewId}(이)라는 이름은 존재합니다.`);
+    check = false;
   } else if (space.test(reviewId) == true) {
     //공백이 있는 경우
-    alert("아이디에는 공백은 사용할 수 없습니다.");
+    alert("공백은 사용할 수 없습니다.");
   } else if (scriptTag.test(reviewId) == true) {
     // 특수문자가 포함된 경우
-    alert("아이디에는 특수문자를 사용할 수 없습니다.");
+    alert("이름에는 특수문자를 사용할 수 없습니다.");
   } else if (reviewId.length > 12) {
     //이름 크기가 12자를 초과한 경우
-    alert("아이디는 12자리 이하여야 합니다.");
+    alert("이름은 12자리 이하여야 합니다.");
   } else if (reviewId.length < 3) {
     //이름 크기가 3자 미만인 경우
-    alert("아이디는 3자리 이상이어야 합니다.");
+    alert("이름은 3자리 이상이어야 합니다.");
   } else {
     check = true;
   }
@@ -50,7 +71,8 @@ export function ReviewIdCheck(reviewId) {
 
 //리뷰 비밀번호 유효성 검사
 export function ReviewPasswordCheck(reviewPassword, reviewId) {
-  let scriptTagKo = /[가-힣|ㄱ-ㅎ|ㅏ-ㅣ]/; //한글들
+  //비밀번호 영문자 + 숫자 + 특수조합(8~25자리 입력) 정규식
+  let scriptTagPwd = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
   let check = false;
 
   if (reviewPassword.length == 0) {
@@ -58,19 +80,14 @@ export function ReviewPasswordCheck(reviewPassword, reviewId) {
     alert("비밀번호를 입력해주세요.");
   } else if (space.test(reviewPassword) == true) {
     //공백이 있는 경우
-    alert("비밀번호에 공백은 사용할 수 없습니다.");
+    alert("공백은 사용할 수 없습니다.");
   } else if (reviewId == reviewPassword) {
     //아이디와 비밀번호가 같을 경우
     alert("비밀번호와 아이디는 같을 수 없습니다.");
-  } else if (scriptTagKo.test(reviewPassword) == true) {
-    //비밀번호가 한글이 있는경우
-    alert("비밀번호는 한글로 작성할 수 없습니다.");
-  } else if (reviewPassword.length < 3) {
-    //비밀번호가 3자 미만인 경우
-    alert("비밀번호는 3자리 이상이어야 합니다.");
-  } else if (reviewPassword.length > 20) {
-    // 비밀번호가 20자 초과한 경우
-    alert("비밀번호는 20자리 이하여야 합니다.");
+  } else if (scriptTagPwd.test(reviewPassword) == false) {
+    alert(
+      "비밀번호는 영문자+숫자+특수문자(!@#$%^*+=-) 조합으로 8-25자리 사용해야 합니다."
+    );
   } else {
     check = true;
   }
@@ -80,6 +97,8 @@ export function ReviewPasswordCheck(reviewPassword, reviewId) {
 //리뷰 텍스트 유효성 검사
 export function ReviewText(reviewText) {
   let check = false;
+  $comment.value = $comment.value.replace("<", "&lt");
+  $comment.value = $comment.value.replace(">", "&gt");
 
   if (reviewText.length == 0) {
     //글이 하나도 없는경우
