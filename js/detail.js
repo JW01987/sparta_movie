49ccb57fab76a125caf74eb5cb6b6484d0ac3b19
yerim <sola4718@gmail.com>
2023-06-07 10:58:12 +0900

    localstorage 저장 출력 삭제기능 구현했습니다.

--- js/detail.js
+++ js/detail.js
@@ -12,6 +12,7 @@ const name = document.querySelector("#name");
 const psw = document.querySelector("#psw");
 const comment = document.querySelector("#comment");
 const movieId = urlParams.get("id");
+
 const options = {
   method: "GET",
   headers: {
@@ -51,10 +52,27 @@ submit.addEventListener("click", () => {
     ReviewPasswordCheck(psw.value, name.value) &&
     ReviewText(comment.value)
   ) {
-    alert("댓글이 등록되었습니다");
+    const commentId = Date.now().toString(); // 고유한 ID 생성
+    const newComment = {
+      id: commentId,
+      name: name.value,
+      password: psw.value,
+      text: comment.value,
+    };
+
+    // 저장된 댓글 가져오기
+    const comments = JSON.parse(localStorage.getItem("comments")) || [];
+    comments.push(newComment);
+
+    // localStorage에 저장
+    localStorage.setItem("comments", JSON.stringify(comments));
+
+    alert("댓글이 등록되었습니다.");
     window.location.reload();
   }
 });
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   //html의 컨텐츠들이 전부 로딩 된 후 실행
@@ -67,6 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((err) => console.error(err)); //위의 과정에서 에러가 나면 이쪽으로
 });
 const printCard = (data) => {
+  console.log(data);
   box.innerHTML = ""; //위에서 저장해둔 box 비우기
   let title = data.title;
   let overview = data.overview;
@@ -85,5 +104,45 @@ const printCard = (data) => {
 
   box.innerHTML += temp_html; //box에 temp_html을 넣기
 
-  //로컬스토리지에서 값 가져오기
-};
+}
+
+
+//버튼을 누르면 해당 id값을 받아서 삭제를 할 수 있게끔 만듦
+const commentbox = document.querySelector(".comment-box");
+let comments = localStorage.getItem("comments");
+if (comments) {
+  comments = JSON.parse(comments);
+  comments.forEach((commentData) => {
+    const { id, name, text } = commentData;
+    const commentHTML = `
+      <div class="comment" data-comment-id="${id}">
+        <p class="comment-name">이름: ${name}</p>
+        <p class="comment-text">리뷰: ${text}</p>
+        <button class="edit-btn">수정</button>
+        <button class="del-btn">삭제</button>
+      </div>
+    `;
+    commentbox.innerHTML += commentHTML;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+//코멘트 삭제
+  const commentBox = document.querySelector(".comment-box");
+  commentBox.addEventListener("click", (event) => {
+    if (event.target.classList.contains("del-btn")) {
+      const commentElement = event.target.closest(".comment");
+      const commentId = commentElement.dataset.commentId;
+      commentElement.remove();
+      alert("리뷰를 삭제하겠습니다.");
+
+     //로컬 저장소에서 삭제
+      const comments = JSON.parse(localStorage.getItem("comments")) || [];
+      const updatedComments = comments.filter((comment) => comment.id !== commentId);
+      localStorage.setItem("comments", JSON.stringify(updatedComments));
+    }
+  });
+});
+
+// ...
